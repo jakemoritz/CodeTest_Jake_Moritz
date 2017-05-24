b6e6998f47e58e6138b421f18d73fbbf8a74266e
jakemoritz1@gmail.com <jakemoritz1@gmail.com>
2017-05-24 18:35:04 -0400

    Launched DialogFragment from FAB with custom layout

--- app/src/main/java/com/domain/codetest_jake_moritz/dialogs/NewPersonDialogFragment.java
+++ app/src/main/java/com/domain/codetest_jake_moritz/dialogs/NewPersonDialogFragment.java
@@ -2,11 +2,13 @@ package com.domain.codetest_jake_moritz.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 import com.domain.codetest_jake_moritz.App;
+import com.domain.codetest_jake_moritz.R;
 import com.domain.codetest_jake_moritz.activities.MainActivity;
 
 import io.realm.Realm;
@@ -26,7 +28,7 @@ public class NewPersonDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
         builder.setTitle("Add a new person")
-                .setMessage("message")
+                .setCancelable(true)
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -44,6 +46,9 @@ public class NewPersonDialogFragment extends DialogFragment {
                         dismiss();
                     }
                 });
+
+        View view = mainActivity.getLayoutInflater().inflate(R.layout.fragment_dialog_new_person, null);
+        builder.setView(view);
 
         return builder.create();
     }
